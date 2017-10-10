6481493fd27a9844af45a07468ce826a249924dc
Tatyana Zakernichnaya <phoenix-12@ya.ru>
2017-10-10 21:10:59 +0300

    fix #3

--- js/main.js
+++ js/main.js
@@ -12,14 +12,14 @@ changeTemplate(numFirstScreen);
 const ARROW_RIGHT = 39;
 const ARROW_LEFT = 37;
 
-const amountTemplates = templates.length - 1;
+const indexLastTemplate = templates.length - 1;
 
-let numScreen = 0;
+let numCurrentScreen = 0;
 document.addEventListener(`keydown`, (e) => {
   if (e.altKey && e.keyCode === ARROW_RIGHT) {
-    numScreen = (numScreen === amountTemplates) ? numFirstScreen : numScreen + 1;
+    numCurrentScreen = (numCurrentScreen === indexLastTemplate) ? numFirstScreen : numCurrentScreen + 1;
   } else if (e.altKey && e.keyCode === ARROW_LEFT) {
-    numScreen = (numScreen === 0) ? amountTemplates : numScreen - 1;
+    numCurrentScreen = (numCurrentScreen === 0) ? indexLastTemplate : numCurrentScreen - 1;
   }
-  changeTemplate(numScreen);
+  changeTemplate(numCurrentScreen);
 });
